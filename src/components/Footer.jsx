e5b48042d3465aa7594caa5ba1f3919abb4225e0
RLjumber <ronaldjumber@gmail.com>
2023-04-25 17:23:34 -0400

    styling and component for singlePageView contentable, routing for the back button needs to be worked out to take user back to where they were exactly, not just the home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,37 @@
 import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer_main">
-            <div className="left_list_div">
-                <ul className="list">
-                    <li>Stuf</li>
-                    <li>and some more stuff</li>
-                    <li>things even</li>
-                    <li>where am i</li>
-                </ul>
-            </div>
-            <div className="right_list_div">
-                <ul className="list">
-                    <li>Stuf</li>
-                    <li>and some more stuff</li>
-                    <li>things even</li>
-                    <li>where am i</li>
-                </ul>
-            </div>
-        </div>
-    )
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer_main">
+      <div className="left_list_div">
+        <ul className="list">
+          <li>
+            <a href="https://github.com/RLjumber">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/ronald-jumber/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="mailto:ronaldjumber@gmail.com">Email</a>
+          </li>
+        </ul>
+      </div>
+      <div className="right_list_div">
+        <ul className="list">
+          <li>Last updated: {currentYear}</li>
+          <li>
+            Built with <a href="https://reactjs.org/">React</a> and pure CSS
+          </li>
+          <li>
+            Designed by{" "}
+            <a href="https://dribbble.com/RLJumber">RLJumber</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/SingleViewPlant.jsx
+++ src/components/SingleViewPlant.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import "./SingleViewPlant.css";
+// import Footer from "./Footer";
 
 const SinglePageView = () => {
+
   const { id } = useParams();
   const [plant, setPlant] = useState(null);
+  const navigate = useNavigate();
+  // const location = useLocation();
 
   useEffect(() => {
     if (id !== undefined) {
@@ -20,14 +25,43 @@ const SinglePageView = () => {
     return <div>Loading...</div>;
   }
 
+  const plantName = 
+      plant.common_name  
+      ? plant.common_name
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+      : null;
+
+  const goBack = () => {
+      // console.log("Hit!");
+      // navigate(location.state?.from || '/'); // using optional chaining operator to get the previous location
+
+      navigate(-1);
+  };
+
   return (
-    <div>
-      <h1>{plant.common_name}</h1>
-      <p>Scientific Name: {plant.scientific_name}</p>
-      <p>Other Name: {plant.other_name}</p>
-      <p>Origin: {plant.origin.map}</p>
-      <img src={plant.default_image.regular_url} alt="Plant" />
-    </div>
+    <div className="single-page-container">
+      <div>
+        <img src={plant.default_image.regular_url} alt="Plant" />
+      </div>
+      <div className="information">
+        <h1 className="common-name">{plantName}</h1>
+        <h4 className="scientific-name"><b>Scientific Name:</b> <em>{plant.scientific_name}</em></h4>
+        {plant.other_name && plant.other_name.length > 0 ? <h4 className="other-name"><b>Other Name:</b> {plant.other_name}</h4> : null}
+        {plant.family ? <p className="family"><b>Family:</b> {plant.family}</p> : null}
+        {plant.origin ? <p className="origin"><b>Origin:</b> {Array.isArray(plant.origin) ? plant.origin.join(', ') : plant.origin}
+        </p> : null}
+        {plant.type ? <p className="type"><b>Type:</b> {plant.type}</p> : null}
+        {plant.cycle ? <p><b>Cycle:</b> {plant.cycle}</p> : null}
+        {plant.dimension ? <p><b>{plant.dimension}</b></p> : null}
+        {plant.watering ? <p><b>Watering:</b> {plant.watering}</p> : null}
+        {plant.attracts && plant.attracts.length > 0 ? <p><b>Attracts:</b> {plant.attracts.join(", ")}</p> : null}
+        {plant.propagation && plant.propagation.length > 0 ? <p><b>Propagation:</b> {Array.isArray(plant.propagation) ? plant.propagation.join(", ") : plant.propagation}</p> : null}
+        {plant.hardiness ? <p><b>Hardiness Zone:</b> <b>Min: </b>{plant.hardiness.min} <b>Max: </b>{plant.hardiness.max}</p> : null}
+        <button onClick={goBack}>Take Me Back!</button>
+  </div>
+</div>
   );
 };
 
